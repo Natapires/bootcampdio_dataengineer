67a224fa5adc51fabb5c0370789575cf1d507f5d
nata <pires.nata1998@gmail.com>
2026-03-09 23:48:24 -0300

    Aula de indentação e blocos

---
+++ Cienciasdedados_Python/Estrutura_condicionais_repeticao/indentacao_blocos.py
@@ -0,0 +1,14 @@
+def sacar(valor):
+    saldo = 1000
+
+    if saldo >= valor:
+        print("Saque com sucesso!")
+        print("Retire as céculas na boca do caixa abaixo.")
+    
+    print("Obrigado por utilizar o banco 24horas!")
+
+def depositar(valor):
+    saldo = 300
+    saldo += valor
+
+sacar(1700)
